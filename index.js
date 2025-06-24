8cd904979bc030f563bbd3da0e9d6f943daa421f
Jennifer Li <jenniferli7437@gmail.com>
2025-06-24 13:04:20 -0400

    writing javascript to calculate sleep cycles

---
+++ index.js
@@ -0,0 +1,23 @@
+const fallAsleepTime = new Date();
+fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+console.log(
+    "You will fall asleep at",
+    fallAsleepTime.toLocaleString("en-US", {
+    timeStyle: "short",
+}));
+
+const wakeUpTime = new Date(fallAsleepTime);
+const wakeUpTimes = [];
+
+for (let i = 1; i <= 6; i++) {
+    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+    const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
+        timeStyle: "short",
+    });
+    wakeUpTimes.push(wakeUpTimeString);
+}
+
+console.log("Wake-up times:", wakeUpTimes.join(", "));
+
+
+
