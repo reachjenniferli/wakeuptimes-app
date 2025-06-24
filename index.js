05dda291527c353ea7ab916b9351cfdd3bd27992
Jennifer Li <jenniferli7437@gmail.com>
2025-06-24 15:27:36 -0400

    Styling HTML Page and Finalizing App

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
-const fallAsleepTime = new Date();
-fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
-console.log(
-    "You will fall asleep at",
-    fallAsleepTime.toLocaleString("en-US", {
-    timeStyle: "short",
-}));
+const clcBtn = document.getElementById("calc-btn");
+const refreshBtn = document.getElementById("refresh-btn");
+const returnBtn = document.getElementById("return-btn");
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+const promptSection = document.getElementById("prompt-section");
+const imageContainer = document.getElementById("img-container");
+const resultSection = document.getElementById("result-section");
 
-const wakeUpTime = new Date(fallAsleepTime);
-const wakeUpTimes = [];
+clcBtn.addEventListener("click", calcWakeUpTimes);
+refreshBtn.addEventListener("click", calcWakeUpTimes);
+returnBtn.addEventListener("click", () => {
+    promptSection.classList.remove("hidden");
+    imageContainer.classList.remove("hidden");
+    resultSection.classList.add("hidden");
+});
 
-for (let i = 1; i <= 6; i++) {
-    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
-    const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
-        timeStyle: "short",
-    });
-    wakeUpTimes.push(wakeUpTimeString);
+function calcWakeUpTimes() {
+    const fallAsleepTime = new Date();
+    fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+
+    const wakeUpTime = new Date(fallAsleepTime);
+    wakeUpHoursDiv.innerHTML = "";
+    for (let i = 1; i <= 6; i++) {
+        wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+        const wakeUpTimeString = wakeUpTime.toLocaleTimeString("en-US", {
+            timeStyle: "short",
+        });
+        const cycleDiv = document.createElement("div");
+        cycleDiv.classList.add("cycle");
+        cycleDiv.setAttribute("id", `cycle-${i}`);
+        cycleDiv.textContent = wakeUpTimeString;
+        wakeUpHoursDiv.appendChild(cycleDiv);
+    }
+
+    promptSection.classList.add("hidden");
+    imageContainer.classList.add("hidden");
+    resultSection.classList.remove("hidden");
 }
-
-console.log("Wake-up times:", wakeUpTimes.join(", "));
-
-
-
